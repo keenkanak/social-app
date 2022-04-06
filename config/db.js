f24666532d273fe237596a125940ecab06eb5a32
Kanak Dullu <kanakdullu@Kanaks-Mac.local>
2022-04-07 01:42:23 +0530

    2nd

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const config = require('config')
+
+const db = config.get('mongoURI')
+
+const connectDb = async ()=>{
+    try{
+        await mongoose.connect(db,{
+            useNewUrlParser:true
+        })
+        console.log("MongoDB connected")
+    }catch(e){
+        console.log("MongoDB connection failed")
+        console.log(e.message)
+        process.exit(1)
+    }
+
+}
+
+module.exports = connectDb
--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 const express = require('express')
-
+const connectDb = require('../config/db')
 const app = express();
 
+
+//Define Port
 const PORT = process.env.PORT || 2003
 
+
+//Connect Database
+connectDb()
+
+
+//API Definition
 app.get('/',(req,res)=>{
     console.log("running");
     res.send("Running")
 })
+
+
+
+
+//Listener
 app.listen(PORT,()=>console.log("Server Started at " + PORT));
